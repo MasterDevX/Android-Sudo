a1bd3a22eb92acdb9a8900197a73b214ff35200e
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-11-16 15:06:13 +0200

    Add files via upload

---
+++ installsudo.sh
@@ -0,0 +1,71 @@
+#!/system/bin/sh
+
+clear
+echo "Android-Sudo v1.0 by MasterDevX. Made in Ukraine."
+echo "-------------------------------------------------"
+crtusr="$(whoami)"
+if [ "$crtusr" == "root" ]; then
+    echo "[sudo] Installing sudo..."
+    printf "[sudo] Enter new password for root (leave empty for none): "
+    read pass
+    if [ "$pass" == "" ]; then
+        nopswd=1
+    else
+        printf "[sudo] Confirm password: "
+        read passconf
+        if [ "$pass" == "$passconf" ]; then
+            nopswd=0
+        else
+            echo "[sudo] Passwords do not match, exiting..."
+            exit 1
+        fi
+    fi
+    echo "[sudo] Remounting system as rw..."
+    mount -o rw,remount /system > /dev/null 2>&1
+    if [ "$?" == "0" ]; then
+        if [ "$nopswd" == "0" ]; then
+            echo "[sudo] Creating configuration file..."
+            echo "$pass" > /etc/sudo.conf
+            chmod 0600 /etc/sudo.conf
+            echo "[sudo] Creating wrapper script..."
+            echo "#!/system/bin/sh
+
+cmd=\"\$@\"
+su -c \"mount -o rw,remount /system > /dev/null 2>&1\"
+su -c \"chmod 0644 /etc/sudo.conf\"
+rootpass=\"\$(cat /etc/sudo.conf)\"
+su -c \"chmod 0600 /etc/sudo.conf\"
+su -c \"mount -o ro,remount /system > /dev/null 2>&1\"
+printf \"[sudo] Enter password for root: \"
+read inputpass
+if [ \"\$inputpass\" == \"\$rootpass\" ]; then
+    su -c \"\$cmd\"
+else
+    echo "[sudo] Authentication failed!"
+    exit 1
+fi" > /system/bin/sudo
+        else
+            echo "[sudo] Creating wrapper script..."
+            echo "#!/system/bin/sh
+
+cmd=\"\$@\"
+su -c \"\$cmd\"" > /system/bin/sudo
+        fi
+        chmod 0755 /system/bin/sudo
+        echo "[sudo] Remounting system as ro..."
+        mount -o ro,remount /system > /dev/null 2>&1
+        if [ "$?" == "0" ]; then
+            :
+        else
+            echo "[sudo] Mounting failed, ignoring..."
+        fi
+        echo "[sudo] Installation finished!"
+    else
+        echo "[sudo] Mounting failed, exiting..."
+        exit 1
+    fi
+else
+    echo "[sudo] Installer should be ran as root, exiting..."
+    exit 1
+fi
+
